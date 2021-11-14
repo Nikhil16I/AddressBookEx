ce3d57813fbfbf65ab3bfd0a6fb2dbec334e207f
Nikhil Patil16 <92767519+Nikhil16I@users.noreply.github.com>
2021-11-14 12:50:15 +0530

    Update AddressBookEx.java

--- src/com/AdressbookInfo/AddressBookEx.java
+++ src/com/AdressbookInfo/AddressBookEx.java
@@ -13,22 +13,24 @@ long PhoneNumber;
 	
 public AddressBookEx(String Name, String Surname, String address, String city, String state, long zip, long contact, String mail) {
 		
- this.FirstName = Name;
- this.LastName = Surname;
- this.Address = address;
- this.City = city;
- this.State = state;
+ this.FirstName = Name; this.LastName = Surname;this.Address = address;this.City = city;this.State = state;this.Email = mail;
  this.ZipCode = zip;
  this.PhoneNumber = contact;
- this.Email = mail;
+ 
 }
   public String toString() {
-	return String.format(FirstName + "," + LastName + "," + Address + "," + City + "," + State + "," + ZipCode + "," + PhoneNumber + "," + Email);
-	}
+return String.format(FirstName + ","  
+		+ LastName + ","
+		+ Address + ","
+                + City + ","
+		+ State + "," 
+                + ZipCode + "," 
+		+ PhoneNumber + ","
+                + Email);
+ 
+  }
 	
 }
-
-		
 
 
 
