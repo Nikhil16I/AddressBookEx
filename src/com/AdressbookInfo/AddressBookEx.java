c9f1365c195cfc114c7bb16b2531ca69a2ed4dd6
[decoderNikhil] <[Nikhilmit777@gmail.com]>
2021-11-14 11:55:29 +0530

    Welcome Message Contacting Adding Info

---
+++ src/com/AdressbookInfo/AddressBookEx.java
@@ -0,0 +1,35 @@
+package com.AdressbookInfo;
+
+public class AddressBookEx {
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to AddressBook");
+}
+	
+String FirstName;String LastName;String Address;String City;String State;String Email;
+long ZipCode;
+long PhoneNumber;
+
+	
+public AddressBookEx(String Name, String Surname, String address, String city, String state, long zip, long contact, String mail) {
+		
+ this.FirstName = Name;
+ this.LastName = Surname;
+ this.Address = address;
+ this.City = city;
+ this.State = state;
+ this.ZipCode = zip;
+ this.PhoneNumber = contact;
+ this.Email = mail;
+}
+  public String toString() {
+	return String.format(FirstName + "," + LastName + "," + Address + "," + City + "," + State + "," + ZipCode + "," + PhoneNumber + "," + Email);
+	}
+	
+}
+
+		
+
+
+
+
